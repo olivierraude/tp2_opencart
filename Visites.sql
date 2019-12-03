e722deb71dfa89fcec6a49d3160abb0d7c7a020d
Raude <e1895083@cmaisonneuve.qc.ca>
2019-12-03 11:20:18 -0500

    Table visites

---
+++ Visites.sql
@@ -0,0 +1,9 @@
+CREATE TABLE tp2_visites(
+    id INT UNSIGNED AUTO_INCREMENT,
+    url_page VARCHAR(150),
+    titre VARCHAR(35),
+    date_visite DATETIME,
+    adresse_IP VARCHAR(50),
+    id_usager INT,
+    PRIMARY KEY(id)
+);

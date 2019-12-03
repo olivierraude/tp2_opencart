6c77648e59b82dec8236173ffddc873454545849
Comeau <e1895410@cmaisonneuve.qc.ca>
2019-12-03 13:22:51 -0500

    controleur visites.php : modification commentaires

--- admin/controller/visites/visites.php
+++ admin/controller/visites/visites.php
@@ -1,24 +1,24 @@
 <?php
     class ControllerVisites extends Controller {
         /**
-         * index = affichage les 15 pages les plus visitées
+         * index = affiche les 15 pages les plus visitées
          */
         public function index(){
-            
+            # code
         }
+
 
         /**
          * ajoute une visite
          */
-
         public function add(){
             # code ...
         }
 
+
         /**
          * affiche toutes les visites
          */
-
         public function afficherLesVisites(){
             # code ...
         }

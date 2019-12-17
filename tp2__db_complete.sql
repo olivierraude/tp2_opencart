6404db2fead7d4fd90ea3e5ecb4820865255a80c
Comeau <e1895410@cmaisonneuve.qc.ca>
2019-12-17 08:08:16 -0500

    tp2__db_complete.sql: renommage adresse_IP par adresse_ip (compatibilité webdev)

--- tp2__db_complete.sql
+++ tp2__db_complete.sql
@@ -3593,7 +3593,7 @@ CREATE TABLE `tp2_visites` (
   `url_page` varchar(150) DEFAULT NULL,
   `titre` varchar(35) DEFAULT NULL,
   `date_visite` datetime DEFAULT NULL,
-  `adresse_IP` varchar(50) DEFAULT NULL,
+  `adresse_ip` varchar(50) DEFAULT NULL,
   `id_usager` int(11) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 

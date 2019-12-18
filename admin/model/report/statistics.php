29fd639072d79742c6678a0bcc5733dc4b84814b
Gazowski <gael_comeau@hotmail.com>
2019-12-17 20:15:47 -0500

    admin/model/report/statistics.php: ajout des requêtes

--- admin/model/report/statistics.php
+++ admin/model/report/statistics.php
@@ -1,5 +1,29 @@
 <?php
 class ModelReportStatistics extends Model {
+
+	/**
+	 * Pour le tp2, ajout des méthodes pour récupérer les données dans le tableau visites
+	 */
+
+	public function getAllVisites()
+    {
+        $query = $this->db->query("SELECT * FROM " . DB_PREFIX . "visites");
+		return $query->rows;
+    }
+    
+    public function getVisite($id_visite)
+    {
+        $this->db->query("SELECT * FROM " . DB_PREFIX . "visites WHERE id = '" . (int)$data['id'] . "' ORDER BY date DESC");
+    }
+    
+    public function getPlusVisite($id_visite)
+    {
+        $this->db->query("SELECT * FROM " . DB_PREFIX . "visites WHERE id = '" . (int)$data['id'] . "' ORDER BY date DESC");
+	}
+	
+
+	/*	function report Statistics	*/
+
 	public function getStatistics() {
 		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "statistics");
 

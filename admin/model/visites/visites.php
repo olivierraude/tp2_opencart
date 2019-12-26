68887098be099442e9bd6dbb9258e635717344ae
Gazowski <gael_comeau@hotmail.com>
2019-12-26 10:45:50 -0500

    visites.php (model admin): essai requete top 15

--- admin/model/visites/visites.php
+++ admin/model/visites/visites.php
@@ -7,12 +7,19 @@ class ModelVisitesVisites extends Model{
 
     public function getAllVisites()
     {
-        $query = $this->db->query("SELECT * FROM " . DB_PREFIX . "visites");
+        $query = $this->db->query("SELECT date_visite, titre , url_page, id_usager, adresse_ip
+                                    FROM " . DB_PREFIX . "visites
+                                    ORDER BY date_visite DESC");
         return $query->rows;
     }
     
     public function getPlusVisite($id_visite)
     {
+        // SELECT `titre`,`url_page`,count(*) as nombre_visites 
+        // FROM `tp2_visites` 
+        // group by `url_page` 
+        // ORDER BY `nombre_visites` 
+        // DESC limit 15
         $this->db->query("SELECT * FROM " . DB_PREFIX . "visites WHERE id = '" . (int)$data['id'] . "' ORDER BY date DESC");
     }
 }

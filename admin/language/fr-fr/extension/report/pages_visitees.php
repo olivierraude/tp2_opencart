23616a5fe3d42477cb14ff52efad81d71a7aa335
Gazowski <gael_comeau@hotmail.com>
2019-12-26 18:38:47 -0500

    pages_visitees.php(lang fr): création

---
+++ admin/language/fr-fr/extension/report/pages_visitees.php
@@ -0,0 +1,13 @@
+<?php
+// Heading
+$_['heading_title']    = '02 - Rapport pages visitées';
+
+// Text
+$_['text_extension']   = 'Extensions';
+
+// Column
+$_['column_titre']      = 'Titre';
+$_['column_date_visite']     = 'Date';
+$_['column_url_page']    = 'URL';
+$_['column_id_usager']   = 'Identifiant usager';
+$_['column_adresse_ip']   = 'IP usager';

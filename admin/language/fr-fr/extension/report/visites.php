2296a558a161a080e15852959bcc58a4ee78b13f
olivierraude <olivierraude@gmail.com>
2020-01-09 12:12:29 -0500

    Finalisation langage fr
    Signed-off-by: olivierraude <olivierraude@gmail.com>

---
+++ admin/language/fr-fr/extension/report/visites.php
@@ -0,0 +1,20 @@
+<?php
+// Heading
+$_['heading_title']    = '15 pages les plus visitées';
+
+// Text
+$_['text_extension']   = 'Extensions';
+$_['text_edit']        = 'Edit Most Visited Report';
+$_['text_success']     = 'Success: You have reset the most visited report!';
+
+// Column
+$_['column_url_page']         = 'URL Page';
+$_['column_titre']            = 'Titre';
+$_['column_views']            = 'Vues';
+
+// Entry
+$_['entry_status']     = 'Status';
+$_['entry_sort_order'] = 'Sort Order';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify most visited report!';

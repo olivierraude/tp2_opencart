7a5b5f0032cf73cf61c5b81dc3fb8ee3e9347485
Gazowski <gael_comeau@hotmail.com>
2020-01-16 20:07:19 -0500

    ajout des fichiers pour le module ajout db visites

---
+++ admin/language/en-gb/extension/module/visites.php
@@ -0,0 +1,9 @@
+<?php
+// Heading
+$_['heading_title'] = 'Visites';
+// Text
+$_['text_visites'] = 'Visites';
+$_['text_instructions'] = 'Instructions';
+$_['text_about'] = 'This VISITES module will install a new table named visites into your Database. It was developed by Olivier and Gael';
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify the visites module!';
---
+++ admin/language/fr-fr/extension/module/visites.php
@@ -0,0 +1,9 @@
+<?php
+// Heading
+$_['heading_title'] = 'Visites';
+// Text
+$_['text_visites'] = 'Visites';
+$_['text_instructions'] = 'Instructions';
+$_['text_about'] = 'Ce module VISITES installera une nouvelle table nommée visites dans votre base de données. Elle a été développée par Olivier et Gael.';
+// Error
+$_['error_permission'] = 'Avertissement: Vous n\'avez pas la permission de modifier le module visites.';
